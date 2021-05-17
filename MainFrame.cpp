01acc0565d8a9d0788233de22c9c5c65ff8ab783
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-05-17 12:46:17 +0300

    Add file menu

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -3,5 +3,14 @@
 MainFrame::MainFrame(const wxString& title, int xPos, int yPos, int width, int height)
     : wxFrame(NULL, -1, title, wxPoint(xPos, yPos), wxSize(width, height))
 {
+    mpMenuBar = new wxMenuBar();
 
+    mpFileMenu = new wxMenu();
+    mpFileMenu->Append(wxID_OPEN, _T("&Open"));
+    mpFileMenu->Append(wxID_SAVE, _T("&Save"));
+    mpFileMenu->Append(wxID_SAVEAS, _T("&Save as"));
+    mpFileMenu->Append(wxID_EXIT, _T("&Quit"));
+    mpMenuBar->Append(mpFileMenu, _T("&File"));
+
+    SetMenuBar(mpMenuBar);
 }
--- MainFrame.h
+++ MainFrame.h
@@ -11,7 +11,8 @@ public:
     MainFrame(const wxString& title, int xPos, int yPos, int width, int height);
 
 private:
-
+    wxMenuBar *mpMenuBar;
+    wxMenu *mpFileMenu;
 };
 
 
